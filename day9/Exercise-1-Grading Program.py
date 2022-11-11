db1570914bec25dd8c31693fb65ee1f49b0e971b
aiden_chen <splash7447@gmail.com>
2022-11-11 18:02:19 +0800

    20221111-day9-add Start.py & End.py & Exercise-1-Grading Program.py & Exercise-2-Dictionary in List.py

---
+++ day9/Exercise-1-Grading Program.py
@@ -0,0 +1,26 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for name in student_scores:
+    if 100 >= student_scores[name] >= 91:
+        student_grades[name] = "Outstanding"
+    elif 90 >= student_scores[name] >= 81:
+        student_grades[name] = "Exceeds Expectations"
+    elif 80 >= student_scores[name] >= 71:
+        student_grades[name] ="Acceptable"
+    else:
+        student_grades[name] = "Fail"
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
+
