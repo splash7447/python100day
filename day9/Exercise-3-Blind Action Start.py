757ffe5fbfac7ca38ca824a320d92343ad3f2119
aiden_chen <splash7447@gmail.com>
2022-11-17 14:26:14 +0800

    20221117-day9-edit some function

--- day9/Exercise-3-Blind Action Start.py
+++ day9/Exercise-3-Blind Action Start.py
@@ -23,8 +23,7 @@ while add_bidding:
     name = input("What is your name?: ")
     price = int(input("What is your bid?: $"))
     bids[name] = price
-    should_continue = input(
-        "Are there any other bidders? Type 'yes or 'no'.\n")
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
     if should_continue == "no":
         add_bidding = False
         find_highest_bidder(bids)
