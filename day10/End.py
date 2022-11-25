a144ebdba0678ba1860a62d5cb62e0c4abe0b5ea
aiden_chen <splash7447@gmail.com>
2022-11-25 17:51:10 +0800

    20221117-day10-Up day10 python

---
+++ day10/End.py
@@ -0,0 +1,10 @@
+# Functions with Outputs
+
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    format_f_name = f_name.title()
+    format_l_name = l_name.title()
+    return f"Result: {format_f_name} {format_l_name}"
+
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
---
+++ day10/Start.py
@@ -0,0 +1,9 @@
+# Functions with Outputs
+
+def format_name(f_name, l_name):
+    format_f_name = f_name.title()
+    format_l_name = l_name.title()
+
+    return f"{format_f_name} {format_l_name}"
+
+print(format_name("aiden", "chen"))
