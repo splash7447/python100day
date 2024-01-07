ae553a4eaea8b535cfecef194734bd098940bf9f
aidenchen <splash7447@gmail.com>
2024-01-07 15:47:08 +0800

    add day14 code with my logical.

--- day14/higher-lower-start ver2.py
+++ day14/higher-lower-start ver2.py
@@ -3,24 +3,19 @@ from game_data import data
 import random
 
 def clear():
-    print("\n" * 20)
+  print("\n" * 20)
 
 def get_random_account():
-  """Get data from random account"""
   return random.choice(data)
 
 def format_data(account):
-  """Format account into printable format: name, description and country"""
   name = account['name']
   description = account['description']
-  contry = account['country']
-  return f"{name}, a {description}, from {contry}"
+  country = account['country']
+  return f'{name}, a{description}, from {country}.'
 
-def check_answer(guess, a_followers, b_followers):
-  """Checks followers against user's guess
-  and returns True if they got it right.
-  Or False if they got it wrong."""
-  if a_followers > b_followers:
+def check_answer(guess, a_followers_counts, b_followers_counts):
+  if a_followers_counts > b_followers_counts:
     return guess == "a"
   else:
     return guess == "b"
@@ -36,25 +31,21 @@ def game():
     account_a = account_b
     account_b = get_random_account()
 
-    while account_a == account_b:
-      account_b = get_random_account()
+  print(f"Compare A: {format_data(account_a)}.")
+  print(vs)
+  print(f"Compare B: {format_data(account_b)}.")
 
-    print(f"Compare A: {format_data(account_a)}.")
-    print(vs)
-    print(f"Against B: {format_data(account_b)}.")
+  guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+  a_followers_counts = account_a['follower_count']
+  b_followers_counts = account_b['follower_count']
+  is_correct = check_answer(guess, a_followers_counts, b_followers_counts)
 
-    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-    a_follower_count = account_a["follower_count"]
-    b_follower_count = account_b["follower_count"]
-    is_correct = check_answer(guess, a_follower_count, b_follower_count)
-
-    clear()
-    print(logo)
-    if is_correct:
-      score += 1
-      print(f"You're right! Current score: {score}")
-    else:
-      game_should_continue = False
-      print(f"Sorry, that's wrong. Final score: {score}")
+  clear()
+  print(logo)
+  if is_correct:
+    score += 1
+    print(f"You're right! Current score: {score}")
+  else:
+    game_should_continue = False
 
 game()
